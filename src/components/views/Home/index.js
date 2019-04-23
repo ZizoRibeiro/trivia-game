34b99ebd7e0665ff5eb02b1d5e681cf823b1b001
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2019-04-23 16:35:56 -0700

    added changes to home

--- src/components/views/Home/index.js
+++ src/components/views/Home/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import { Container, Title, Presentation, Question, ButtonWrapper, ButtonText } from './styles';
+import { Presentation } from './styles';
+import { Container } from '../../shared/Container';
+import { Title } from '../../shared/Title';
+import { Question } from '../../shared/Question';
+import { ButtonWrapper, ButtonText } from '../../shared/Button';
 
 const Home = props => (
   <Container>
     <Title> Welcome to the Challenge!!</Title>
     <Presentation>You will be Presented with 10 True or False questions</Presentation>
-    <Question>Can you score 100%</Question>
+    <Question>Can you score 100%?</Question>
     <ButtonWrapper onPress={() => props.navigation.navigate('Quizz')}>
       <ButtonText>BEGIN!!</ButtonText>
     </ButtonWrapper>
