e213a25ad524ad0dea1126c2512ea98e5cdca195
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2019-04-23 16:37:31 -0700

    added changes to the quizz screen

--- src/components/views/Quizz/index.js
+++ src/components/views/Quizz/index.js
@@ -1,30 +1,70 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
+
+import { flow } from 'lodash';
 
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { bindActionCreators, compose } from 'redux';
 import { actions } from './redux';
+
 import Quizz from './Quizz';
+import { Container as FlexContainer } from '../../shared/Container';
+
 export { reducer } from './redux';
 
-class Container extends React.Component {
+type Props = {
+  navigation: object,
+  answersCount: number,
+  questions: [object],
+  nextQuestion: [object],
+  loading: boolean,
+  storeAnswer: (answer: string, question: Object) => void,
+};
+
+class Container extends React.Component<Props> {
   componentDidMount() {
     this.props.fetchQuestions();
   }
 
   render() {
-    console.log(this.props);
-    const { navigation, questions, loading } = this.props;
-    console.log(questions[0]);
+    const {
+      navigation,
+      answersCount,
+      questions,
+      nextQuestion = {},
+      loading,
+      storeAnswer,
+    } = this.props;
     if (loading) {
-      return null;
+      return (
+        <FlexContainer>
+          <ActivityIndicator size="large" color="#0DFFC9" />
+        </FlexContainer>
+      );
     }
-    return <Quizz navigation={navigation} question={questions[0]} />;
+    let onAnswer = storeAnswer;
+    if (answersCount + 1 === questions.length) {
+      onAnswer = compose(
+        () => navigation.navigate('Results'),
+        storeAnswer
+      );
+    }
+    return (
+      <Quizz
+        navigation={navigation}
+        question={nextQuestion}
+        answersCount={answersCount}
+        onAnswer={onAnswer}
+      />
+    );
   }
 }
 
 const mapStateToProps = state => ({
   loading: state.loading,
   questions: state.questions,
+  nextQuestion: state.questions[state.answers.length],
+  answersCount: state.answers.length,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
