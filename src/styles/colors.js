9cb42e275800d45e43b215c031b540e2273f08fd
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2019-04-22 20:48:19 -0700

    aded colors,fonts, metrics for  style

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+export default {
+  white: '#FFFFFF',
+  yellow: '#F8E71C',
+  lighter: '#EEE',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+  primary: '#09274A',
+  secondary: '#35A0CD',
+  background: '#F5F5F5',
+  success: '#0DFFC9',
+  danger: '#D0021B',
+  darkBlue: '#19314D',
+};
